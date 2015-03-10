9e5b12d5194c49098351d232e8afa78f16e257e9
Ryan Culpepper <ryanc@racket-lang.org>
2015-03-09 23:22:33 -0400

    ... working on asts

--- ast.rkt
+++ ast.rkt
@@ -1,8 +1,107 @@
 ;; Run-time SQL ast structures
 
 #lang racket/base
-(require racket/string)
+(require racket/string
+         (rename-in racket/match [match-define defmatch])
+         racket/generic
+         racket/class
+         racket/format)
 (provide (all-defined-out))
+
+;; ============================================================
+;; Abstract Nonterminals
+
+;; table-ref
+;; table-expr
+;; scalar-expr
+
+(define-generics scalar-expr
+  (emit-scalar-expr scalar-expr))
+
+(define-generics table-expr
+  (emit-table-expr table-expr))
+
+(define-generics table-ref
+  (emit-table-ref table-ref)
+  #:defaults
+  ([table-expr?
+    (define (emit-table-ref tr) (~a "(" (emit-table-expr tr) ")"))]))
+
+;; ----------------------------------------
+
+(struct table-ref:id (id)
+        #:method gen:table-ref
+        [(define (emit-table-ref t)
+           (emit-id (table-ref:id-id t)))])
+(struct table-ref:as (e rangevar)
+        #:method gen:table-ref
+        [(define (emit-table-ref t)
+           (defmatch (table-ref:as e rangevar) t)
+           (~a (emit-table-expr e) " AS " (emit-id rangevar)))])
+(struct table-expr:join0 (type t1 t2)
+        #:method gen:table-expr
+        [(define (emit-table-expr t)
+           (defmatch (table-expr:join0 type t1 t2) t)
+           (~a (emit-table-ref t1)
+               (case type
+                 [(cross) " CROSS JOIN "]
+                 [(union) " UNION JOIN "])
+               (emit-table-ref t2)))])
+(struct table-expr:join (type t1 t2 on)
+        #:method gen:table-expr
+        [(define (emit-table-expr t)
+           (defmatch (table-expr:join type t1 t2 on) t)
+           (~a "("
+               (emit-table-ref t1)
+               (case on
+                 [(natural) " NATURAL"]
+                 [else ""])
+               (case type
+                 [(inner) " INNER JOIN "]
+                 [(left)  " LEFT OUTER JOIN "]
+                 [(right) " RIGHT OUTER JOIN "]
+                 [(full)  " FULL OUTER JOIN "])
+               (emit-table-ref t2)
+               (cond [(list? on)
+                      (~a " USING (" (string-join (map emit-id on) ",") ")")]
+                     [(scalar-expr? on)
+                      (~a " ON " (emit-scalar-expr on))]
+                     [else ""])
+               ")"))])
+(struct table-expr:set-op (type t1 t2 opt)
+        #:method gen:table-expr
+        [(define (emit-table-expr t)
+           (defmatch (table-expr:set-op type t1 t2 opt) t)
+           (~a "("
+               (emit-table-ref t1)
+               (case type
+                 [(union) " UNION "]
+                 [(except) " EXCEPT "]
+                 [(intersect) " INTERSECT "])
+               (case on
+                 [(all) "ALL "]
+                 [else ""])
+               (emit-table-ref t2)
+               ")"))])
+(struct table-expr:select (select)
+        #:method gen:table-expr
+        [(define (emit-table-expr t)
+           (~a "(" (emit-select-stmt (table-expr:select-select t)) ")"))])
+(struct table-expr:values (rows)
+        #:method gen:table-expr
+        [(define (emit-table-expr t)
+           (~a "VALUES "
+               (string-join
+                (for/list ([row rows])
+                  (~a "(" (string-join (map emit-scalar-expr row) ",") ")"))
+                ",")))])
+
+
+
+;; ============================================================
+;; OLD STUFF
+
+
 
 ;; A UID (unqualified identifier) is one of
 ;;  - string                            ;; dots interpreted literally (if possible)
