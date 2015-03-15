b8253f2880810dafdeed4113da316667e6a1a45f
Ryan Culpepper <ryanc@racket-lang.org>
2015-03-14 23:58:58 -0400

    move impl files to private subdir

--- main.rkt
+++ main.rkt
@@ -1,9 +1,3 @@
 #lang racket/base
-(require "ast.rkt"
-         "parse.rkt"
-         "emit.rkt"
-         "syntax.rkt")
-(provide (all-from-out "ast.rkt")
-         (all-from-out "parse.rkt")
-         (all-from-out "emit.rkt")
-         (all-from-out "syntax.rkt"))
+(require "private/syntax.rkt")
+(provide (all-from-out "private/syntax.rkt"))
