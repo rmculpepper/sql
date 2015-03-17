49c4c9accc0b02253aa8b2262eaeb231971a1bde
Ryan Culpepper <ryanc@racket-lang.org>
2015-03-16 23:14:36 -0400

    export ast predicates

--- main.rkt
+++ main.rkt
@@ -1,3 +1,10 @@
 #lang racket/base
-(require "private/syntax.rkt")
-(provide (all-from-out "private/syntax.rkt"))
+(require "private/syntax.rkt"
+         "private/ast.rkt")
+(provide (all-from-out "private/syntax.rkt")
+         ident?
+         name?
+         scalar-expr?
+         table-ref?
+         table-expr?
+         statement?)
